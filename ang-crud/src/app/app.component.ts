e327d9180e4797995d03cbc23297e638d549095e
carlos ladron de guevara <carlosldgm@gmail.com>
2020-03-30 01:28:27 -0300

    se agrega accion de agregar o actualizar una persona

--- ang-crud/src/app/app.component.ts
+++ ang-crud/src/app/app.component.ts
@@ -6,44 +6,53 @@ import { FormControl } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-  
+
 
 export class AppComponent {
   // form: FormGroup;
-  msg: string;  
-  title :string= 'ang-crud';
-  employees =[
-    {rut:"12463391-5",name:"carlos", position:"developer",email:"carlos@gmail.com"},
-    {rut:"1-9",name:"nicolas", position:"gerente",email:"nicolas@gmail.com"},
-    {rut:"5576777-7",name:"martin", position:"jefe",email:"martin@gmail.com"}
+  msg: string;
+  title: string = 'ang-crud';
+  employees = [
+    { rut: "12463391-5", name: "carlos", position: "developer", email: "carlos@gmail.com" },
+    { rut: "1-9", name: "nicolas", position: "gerente", email: "nicolas@gmail.com" },
+    { rut: "5576777-7", name: "martin", position: "jefe", email: "martin@gmail.com" }
   ]
 
-  model:any = {};
-  model2:any = {};
-  addEmployee(): void{
+  model: any = {};
+  addEmployee(): void {
+    for (let i = 0; i < this.employees.length; i++) {
+      const element = this.employees[i];
+      if (this.employees[i].rut == this.model.rut) {
+        this.updateEmployee(i);
+        this.model = {};
+        this.msg = "Usuario Modificado!"
+        return;
+      }
+    }
     this.employees.push(this.model);
     this.model = {};
-    //this.formulario.reset();
+    this.msg = "Usuario Agregado!"
   }
 
   deleteEmployee(i): void {
     console.log("i " + i);
-    this.employees.splice(i,1);
+    this.employees.splice(i, 1);
     this.clear();
+    this.msg = "Usuario Eliminado!"
   }
-  editEmployee(i):void{
+  editEmployee(i): void {
     this.model = this.employees[i];
   }
 
-  updateEmployee():void{
-    
+  updateEmployee(pos): void {
+    this.employees[pos] = this.model;
   }
 
-  closeAlert(): void{
+  closeAlert(): void {
     this.msg = null;
   }
 
-  clear(): void{
+  clear(): void {
     this.model = {};
   }
 }
