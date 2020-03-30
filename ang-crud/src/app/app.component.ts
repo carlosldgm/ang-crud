1e18db9bb7ba06199f39b38fd3b38768947c3d79
carlos ladron de guevara <carlosldgm@gmail.com>
2020-03-30 00:57:28 -0300

    se saca seccion lateral de edit y se dejan funcionando los botones

--- ang-crud/src/app/app.component.ts
+++ ang-crud/src/app/app.component.ts
@@ -13,36 +13,37 @@ export class AppComponent {
   msg: string;  
   title :string= 'ang-crud';
   employees =[
-    {name:"carlos", position:"developer",email:"carlos@gmail.com"},
-    {name:"nicolas", position:"gerente",email:"nicolas@gmail.com"},
-    {name:"martin", position:"jefe",email:"martin@gmail.com"}
+    {rut:"12463391-5",name:"carlos", position:"developer",email:"carlos@gmail.com"},
+    {rut:"1-9",name:"nicolas", position:"gerente",email:"nicolas@gmail.com"},
+    {rut:"5576777-7",name:"martin", position:"jefe",email:"martin@gmail.com"}
   ]
 
   model:any = {};
-
+  model2:any = {};
   addEmployee(): void{
-    // alert(this.model.name);
     this.employees.push(this.model);
+    this.model = {};
     //this.formulario.reset();
   }
 
   deleteEmployee(i): void {
     console.log("i " + i);
     this.employees.splice(i,1);
+    this.clear();
   }
-  editEmployee():void{
-
+  editEmployee(i):void{
+    this.model = this.employees[i];
   }
 
   updateEmployee():void{
-
+    
   }
 
   closeAlert(): void{
     this.msg = null;
   }
 
-  limpiaCampos(): void{
-    this.model.name = "";
+  clear(): void{
+    this.model = {};
   }
 }
